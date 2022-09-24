b752465c4ff95af4416542b162c9e607c64ed7b4
AidanTrent <84699533+AidanTrent@users.noreply.github.com>
2022-09-24 13:51:14 -0500

    add base files

---
+++ saveManager.h
@@ -0,0 +1,30 @@
+#ifndef SAVEMANAGER_H
+#define SAVEMANAGER_H
+#include <stdint.h>
+
+typedef struct Node{
+	struct Node* next;
+	uint16_t dfBytes;
+	uint8_t data[];
+} Node;
+
+typedef struct{
+	Node* head;
+	Node* cur;
+} LList;
+
+// Make a list containing one node with given params
+LList* makeList(uint16_t dfBytes, uint8_t* data);
+
+// Add a node of given params to top of list
+Node* push(LList* list, uint16_t dfBytes, uint8_t* data);
+
+// Remove node from list
+void freeNode(LList* list, Node* node);
+
+// Saves an EntityList as a binary file. Returns 1 on failure, 0 success
+int saveList(LList* list, char saveName[]);
+
+// Loads a saved EntityList from a file
+LList* loadSave(char saveName[]);
+#endif
