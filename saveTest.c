206e774f32228ab0c14bcd427806c06fe9ca00e1
AidanTrent <84699533+AidanTrent@users.noreply.github.com>
2022-09-26 13:49:54 -0500

    add more tests

--- saveTest.c
+++ saveTest.c
@@ -2,14 +2,17 @@
  * For testing saveManager
  */
 
-#pragma pack(1)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 #include "saveManager.h"
 
+#pragma pack(1)
 typedef struct{
-	uint16_t nodeNum;
+	char rep;
+	int32_t y;
+	int32_t x;
 	uint8_t state;
 } TestData;
 
@@ -18,9 +21,12 @@ LList* populateSave(int nodes){
 	uint8_t* raw = malloc(sizeof(TestData));
 	TestData test;
 
-	for (int i = 1; i <= nodes; i++){
-		test.nodeNum = i;
+	for (int i = -20; i <= nodes; i++){
+		test.rep = '#';
+		test.y = i;
+		test.x = i;
 		test.state = 255;
+
 		memcpy(raw, &test, sizeof(TestData));
 		if (list == NULL){
 			list = makeList(sizeof(TestData), raw);
@@ -36,7 +42,7 @@ LList* populateSave(int nodes){
 }
 
 int main(void){
-	LList* list = populateSave(3);
+	LList* list = populateSave(100);
 	if (list == NULL){
 		return(EXIT_FAILURE);
 	}
@@ -45,18 +51,27 @@ int main(void){
 		return(EXIT_FAILURE);
 	}
 
-
 	LList* list2;
 	list2 = loadSave("entityListTest.sav");
-	//freeList(list);
 
-	//printf("%d\n", freeNode(list2, list->head));
-	//printf("%d\n", freeNode(list2, list2->head));
+	TestData loadedStruct;
+	memcpy(&loadedStruct, list2->head->data, sizeof(TestData));
+	printf("list2 head data...\n");
+	printf("rep = %u\n", loadedStruct.rep);
+	printf("y = %" PRIu32 "\n", loadedStruct.y);
+	printf("x = %" PRIu32 "\n", loadedStruct.x);
+	printf("state = %" PRIu8 "\n\n", loadedStruct.state);
+
+
 	if (saveList(list2, "entityListTest2.sav") == 1){
 		return(EXIT_FAILURE);
 	}
+	printf("freeNode that DNE failure = %d\n\n", freeNode(list2, list->head));
+	printf("freeNode that does exist failure = %d\n\n", freeNode(list2, list2->head));
+	freeList(list);
 	freeList(list2);
 
 
 	return EXIT_SUCCESS;
 }
+
